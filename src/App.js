06fd06a4af474f3fb5e8ce5debe1da0d020b8d27
angelicamolero <angelicamoleror8@gmail.com>
2020-12-15 12:01:06 -0300

    adding form doc

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import Header from './components/Header';
+import Form from './components/Form';
 
 function App() {
   return (
@@ -8,7 +9,16 @@ function App() {
         titulo="Weather React App"
       />
       <div className="container-form">
-        
+        <div className="container">
+          <div className="row">
+            <div className="col m6 s12">
+              <Form/>
+            </div>
+            <div className="col m6 s12">
+              2
+            </div>
+          </div>
+        </div>
       </div>
     </Fragment>
   );
---
+++ src/components/Form.js
@@ -0,0 +1,9 @@
+import React, { Fragment } from 'react';
+
+const Form = ({titulo}) => {
+    return(
+        <h1>Form</h1>
+    );
+}
+
+export default Form;
